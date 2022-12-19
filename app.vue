ce820472abd7a112136136158800151a1a6c629f
ArunGovil <a.arungovil@gmail.com>
2022-12-19 18:08:16 +0530

    controls/undo&redo

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
+<script>
+export default {
+  components: {},
+
+  setup() {
+    const content = ref();
+
+    const printContent = () => {
+      console.log(content.value);
+    };
+
+    return { printContent, content };
+  },
+};
+</script>
+
 <template>
-  <div class="flex mt-8 justify-center">
+  <div class="flex flex-col w-full items-center justify-center p-4">
     <TextEditor />
+    <div class="flex flex-col items-end max-w-4xl w-full">
+      <button
+        @click="printContent"
+        type="button"
+        class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none"
+      >
+        Print
+      </button>
+    </div>
   </div>
 </template>
--- components/TextEditor.vue
+++ components/TextEditor.vue
@@ -13,6 +13,8 @@ export default {
       extensions: [StarterKit],
     });
 
+    onBeforeUnmount(() => editor.value.destroy());
+
     return { editor };
   },
 };
@@ -20,24 +22,59 @@ export default {
 
 <template>
   <div
-    class="border border-gray-200 p-2 rounded-lg flex flex-col w-full max-w-4xl"
+    class="border border-gray-200 p-2 rounded-lg flex flex-col w-full max-w-4xl m-4"
   >
-    <div class="flex w-full rounded-lg p-2 gap-2">
-      <button
-        @click="editor.chain().focus().toggleBold().run()"
-        class="bg-white border border-gray-200 p-2 rounded-md"
-        :class="editor?.isActive('bold') && 'bg-gray-200'"
-      >
-        <img class="w-5 h-5" src="../assets/icons/bold.svg" />
-      </button>
+    <div class="flex w-full justify-between rounded-lg p-2">
+      <div class="flex gap-2">
+        <button
+          @click="editor.chain().focus().toggleBold().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :class="editor?.isActive('bold') && 'bg-gray-200'"
+        >
+          <img class="w-5 h-5" src="../assets/icons/bold.svg" />
+        </button>
 
-      <button
-        @click="editor.chain().focus().toggleItalic().run()"
-        class="bg-white border border-gray-200 p-2 rounded-md"
-        :class="editor?.isActive('italic') && 'bg-gray-200'"
-      >
-        <img class="w-5 h-5" src="../assets/icons/italic.svg" />
-      </button>
+        <button
+          @click="editor.chain().focus().toggleItalic().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :class="editor?.isActive('italic') && 'bg-gray-200'"
+        >
+          <img class="w-5 h-5" src="../assets/icons/italic.svg" />
+        </button>
+
+        <button
+          @click="editor.chain().focus().toggleBulletList().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :class="editor?.isActive('bulletList') && 'bg-gray-200'"
+        >
+          <img class="w-5 h-5" src="../assets/icons/list.svg" />
+        </button>
+
+        <button
+          @click="editor.chain().focus().toggleOrderedList().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :class="editor?.isActive('orderedList') && 'bg-gray-200'"
+        >
+          <img class="w-5 h-5" src="../assets/icons/list.svg" />
+        </button>
+      </div>
+      <div class="flex gap-2">
+        <button
+          @click="editor.chain().focus().undo().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :disabled="!editor?.can().chain().focus().undo().run()"
+        >
+          <img class="w-5 h-5" src="../assets/icons/arrow-left.svg" />
+        </button>
+
+        <button
+          @click="editor.chain().focus().redo().run()"
+          class="bg-white border border-gray-200 p-2 rounded-md"
+          :disabled="!editor?.can().chain().focus().redo().run()"
+        >
+          <img class="w-5 h-5" src="../assets/icons/arrow-right.svg" />
+        </button>
+      </div>
     </div>
     <EditorContent class="p-2 bg-white" :editor="editor" />
   </div>
