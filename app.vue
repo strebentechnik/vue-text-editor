81a330db23c0a91ac7fb4d7385350b4528f28586
Manuashokan18 <manu@strebentechnik.com>
2023-01-31 16:28:00 +0530

    responsive part is done

--- app.vue
+++ app.vue
@@ -19,14 +19,14 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col w-full items-center justify-center p-4">
+  <div class="flex flex-col w-full items-center justify-center p-4 ">
     <h1 class="text-2xl font-medium text-gray-700">Rich Text Editor</h1>
     <TextEditor :state="state" />
     <div class="flex flex-col items-end max-w-4xl w-full">
       <button
         @click="printContent"
         type="button"
-        class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none"
+        class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 mr-5 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none"
       >
         Console
       </button>
--- components/TextEditor.vue
+++ components/TextEditor.vue
@@ -18,7 +18,7 @@ export default {
     });
 
     onBeforeUnmount(() => editor.value.destroy());
-    
+
     return { editor, props };
   },
 };
@@ -26,37 +26,37 @@ export default {
 
 <template>
   <div
-    class="border border-gray-200 p-2 rounded-lg flex flex-col w-full max-w-4xl m-4"
+    class="flex flex-col border border-gray-200 p-2 rounded-lg w-full md:max-w-4xl m-4"
   >
-    <div class="flex w-full justify-between rounded-lg p-2">
-      <div class="flex gap-2">
+    <div class="flex justify-between w-full rounded-lg p-2">
+      <div>
         <button
           @click="editor.chain().focus().toggleBold().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
           :class="editor?.isActive('bold') && 'bg-gray-200'"
         >
-          <img class="w-5 h-5" src="../assets/icons/bold.svg" />
+          <img class="h-5 w-5" src="../assets/icons/bold.svg" />
         </button>
 
         <button
           @click="editor.chain().focus().toggleItalic().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
           :class="editor?.isActive('italic') && 'bg-gray-200'"
         >
-          <img class="w-5 h-5" src="../assets/icons/italic.svg" />
+          <img class="h-5 w-5" src="../assets/icons/italic.svg" />
         </button>
 
         <button
           @click="editor.chain().focus().toggleCode().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
           :class="editor?.isActive('code') && 'bg-gray-200'"
         >
-          <img class="w-5 h-5" src="../assets/icons/code.svg" />
+          <img class="h-5 w-5" src="../assets/icons/code.svg" />
         </button>
 
         <button
           @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="bg-white border border-gray-200 p-2 rounded-md m-2"
           :class="editor?.isActive('heading', { level: 1 }) && 'bg-gray-200'"
         >
           <p class="h-5 w-5">H1</p>
@@ -70,42 +70,53 @@ export default {
         </button>
         <button
           @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="bg-white border border-gray-200 p-2 rounded-md m-2"
           :class="editor?.isActive('heading', { level: 3 }) && 'bg-gray-200'"
         >
           <p class="h-5 w-5">H3</p>
         </button>
+        <!-- link -->
+        <button
+          @click="editor.chain().focus().toggleLink().run()"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
+          :class="editor?.isActive('link') && 'bg-gray-200'"
+        >
+          <img class="h-5 w-5" src="../assets/icons/link.svg" />
+        </button>
+        <!-- link -->
+
         <button
           @click="editor.chain().focus().toggleBulletList().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
           :class="editor?.isActive('bulletList') && 'bg-gray-200'"
         >
-          <img class="w-5 h-5" src="../assets/icons/list.svg" />
+          <img class="h-5 w-5" src="../assets/icons/list.svg" />
         </button>
 
         <button
           @click="editor.chain().focus().toggleOrderedList().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="border border-gray-200 bg-white rounded-md p-2 m-1"
           :class="editor?.isActive('orderedList') && 'bg-gray-200'"
         >
-          <img class="w-5 h-5" src="../assets/icons/o-list.svg" />
+          <img class="h-5 w-5" src="../assets/icons/o-list.svg" />
         </button>
       </div>
-      <div class="flex gap-2">
+
+      <div class="mt-2 md:flex md:gap-2">
         <button
           @click="editor.chain().focus().undo().run()"
           class="bg-white border border-gray-200 p-2 rounded-md"
           :disabled="!editor?.can().chain().focus().undo().run()"
         >
-          <img class="w-5 h-5" src="../assets/icons/arrow-left.svg" />
+          <img class="h-5 w-5" src="../assets/icons/arrow-left.svg" />
         </button>
 
         <button
           @click="editor.chain().focus().redo().run()"
-          class="bg-white border border-gray-200 p-2 rounded-md"
+          class="bg-white border border-gray-200 p-2 rounded-md mt-5 md:mt-0"
           :disabled="!editor?.can().chain().focus().redo().run()"
         >
-          <img class="w-5 h-5" src="../assets/icons/arrow-right.svg" />
+          <img class="h-5 w-5" src="../assets/icons/arrow-right.svg" />
         </button>
       </div>
     </div>
